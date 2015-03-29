8a6c879b4b2783451d059608be295afeaae46c10
Saeed <bahmanabadi@elenoon.ir>
2015-03-29 16:02:44 +0430

    Create StdAfx.cc

---
+++ StdAfx.cc
@@ -0,0 +1,7 @@
+// stdafx.cc : source file that includes just the standard includes
+  
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
